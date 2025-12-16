a34d71cb32cead32d1145cf8dbe4d190d33815da
SDInkHearts <SDInHearts@Gmail.Com>
2025-12-16 17:39:06 +0600

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://sudo-proxy.lustyflix.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Nzg5Y2VhZDY5ODc5YzQ0Y2E5MDM4Njk4NTI1Y2IzOSIsIm5iZiI6MTYzNjEyNDg2Ny43ODMsInN1YiI6IjYxODU0OGMzMDc1Mjg4MDA4ZDBlNDE1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PJbuimwoh68UwIExHC5NWcDi1J2Itkk9SklV6ZkT6JE",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://server.fifthwit.net",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
